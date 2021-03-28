613fab3bd2cac4658818023ba170d4fd8634a6c2
Jan Müller <jan.m.muller@me.com>
2021-03-28 18:25:36 +0200

    start adding system specifics

---
+++ my_system.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from charactersheet.sheet import Charactersheet
+
+def create_charactersheet():
+    mapping = {
+        "Stärke": [
+            "Klettern & Springen",
+            "Schwimmen",
+            "Zähigkeit",
+            "Einschüchtern",
+            "Fauskampf",
+        ],
+        "Geschicklichkeit": [
+            "Reiten",
+            "Rennen",
+            "Heimlichkeit",
+            "Schlösser Knacken",
+            "Parieren",
+            "Reflexe",
+        ],
+        "Intelligenz": [
+            "Tränke",
+            "Erste Hilfe",
+            "Improvisieren",
+            "Kulturelles Wissen",
+            "Naturkunde",
+            "Gerätebedienung"
+        ],
+        "Sinnesstärke": [
+            "Wahrnehmung",
+            "Spuren lesen",
+            "Menschenkenntnis"
+        ],
+        "Soziales": [
+            "Überreden",
+            "Feilschen",
+            "Täuschen und Lügen",
+            "Willensstärke"
+        ],
+        "Fernkampf": [
+            "Wurfwaffen",
+            "Bogen",
+            "Armbrüste"
+        ],
+        "Einhandwaffen": [
+            "Dolche",
+            "Schwerter",
+            "Degen",
+            "Käulen"
+        ],
+        "Zweihandwaffen": [
+            "Breitschwerter",
+            "Stangenwaffen",
+        ],
+    }
+
+    return Charactersheet(mapping)
